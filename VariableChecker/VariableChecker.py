9e8274061db4934db68514e670ea4deb0dd9467c
cjgould <cjgould@ncsu.edu>
2018-10-01 19:54:28 -0400

    Added documentation

--- VariableChecker/VariableChecker.py
+++ VariableChecker/VariableChecker.py
@@ -3,9 +3,9 @@ import re
 
 connection = sqlite3.connect('Variables')
 c = connection.cursor()
-#c.execute('''DROP TABLE variables''')
 
 
+# reads through the entirety of the file and pulls variable names out and prints them with the students name
 def read_variables_in_code(file_name, student_name):
     with open(file_name) as f:
         lines = f.readlines()
@@ -37,17 +37,17 @@ def read_variables_in_code(file_name, student_name):
                                 VALUES ("{}", "{}");'''.format(student_name, varName))
     connection.commit()
 
-
+# creates the table if it has been cleared
 def create_table():
     c.execute('''CREATE TABLE variables
                        (username VARCHAR(30), variableName VARCHAR(60))''')
 
-
+# Clears all variables from the list
 def clear_table():
     c.execute("DROP TABLE variables")
     connection.commit()
 
-
+# prints all current variables in the list
 def print_variables():
     c.execute("SELECT * FROM variables")
     list = c.fetchall()
@@ -55,3 +55,9 @@ def print_variables():
         print(list[i][0])
         print(list[i][1])
 
+
+# return a 2d array with all of the variables and names associated with them
+def get_variable_list():
+    c.execute("SELECT * FROM variables")
+    list = c.fetchall()
+    return list;
