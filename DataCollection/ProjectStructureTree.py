bfbb6079cc14419c8b0072b3d31a588fa87dd18e
cjgould <cjgould@ncsu.edu>
2018-10-03 23:34:14 -0400

    Documented ProjectStructureTree

--- DataCollection/ProjectStructureTree.py
+++ DataCollection/ProjectStructureTree.py
@@ -1,5 +1,6 @@
-
+# Overall project node, can have multiple for a whole class and compare them to check equality
 class ProjectNode:
+    # Constructor for the project node
     def __init__(self, project_name, class_names):
         self.project_name = project_name
         self.class_list = []
@@ -8,12 +9,16 @@ class ProjectNode:
             self.class_list.append(ClassTreeNode(class_names[i], self))
             self.class_numbers = self.class_numbers + 1;
 
+    # Adds a class to the project node
     def add_class_node(self, class_name, method_names=None, structures_list=None):
         self.class_list.append(ClassTreeNode(class_name, self, method_names, structures_list))
 
 
+# Class node used for each individual java class
 class ClassTreeNode:
 
+    # Constructor for the class node, will add in methods if provided, also holds any dependencies for the class if
+    # provided
     def __init__(self, class_name, project_node, method_names=None, structures_list=None, dependencies_list=None):
         self.class_name = class_name
         self.project_node = project_node
@@ -23,6 +28,7 @@ class ClassTreeNode:
             for i in range(len(method_names)):
                 self.method_node_list.append(MethodTreeNode(method_names[i], self, structures_list))
 
+    # Adds another method to the class, will add in structures too if not null
     def add_method_node(self, method_names, structures_list=None):
         if self.method_node_list is None:
             self.method_node_list = []
@@ -30,6 +36,7 @@ class ClassTreeNode:
         else:
             self.method_node_list.append(MethodTreeNode(method_names, self))
 
+    # Adds in dependencies to the class if needed
     def add_dependency(self, dependency_name, project_node, method_names, structures_list):
         if self.dependencies_list is None:
             self.dependencies_list = []
@@ -38,8 +45,10 @@ class ClassTreeNode:
             self.dependencies_list.append(ClassTreeNode(dependency_name, project_node, method_names, structures_list))
 
 
+# Method node class that holds information about each method
 class MethodTreeNode:
 
+    # Constructor for the method node class, will add in structures if provided
     def __init__(self, method_name, class_node, structures_list=None):
         self.method_name = method_name
         self.class_node = class_node
@@ -48,6 +57,7 @@ class MethodTreeNode:
             for i in range(len(structures_list)):
                 self.structures_nodes.append(StructureTreeNode(structures_list[i], self))
 
+    # Adds a structure node to the list
     def add_structure_node(self, structure_name):
         if self.structures_nodes is None:
             self.structures_nodes = []
@@ -56,13 +66,19 @@ class MethodTreeNode:
             self.structures_nodes.append(StructureTreeNode(structure_name, self))
 
 
+# Structure node class can also hold an instance of itself if the structure is nested
 class StructureTreeNode:
 
+    # Constructor for the Structure node cladd, will add inner structures if provided
     def __init__(self, structure_name, method_node, inner_structures=None):
         self.structure_name = structure_name
         self.method_node = method_node
-        self.inner_structures = inner_structures
+        if inner_structures is not None:
+            for i in range(len(inner_structures)):
+                self.inner_structures = []
+                self.inner_structures.append(StructureTreeNode(inner_structures[i], self.method_node))
 
+    # Adds an inner structure to the list
     def add_inner_structure(self, inner_structure_name):
         if self.inner_structures is None:
             self.inner_structures = []
